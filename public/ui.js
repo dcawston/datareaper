1ebc4a89b21f1d77c92f5146b9665f7b824a377d
m-hex <mattyhex@mattyhex.net>
2015-06-17 14:05:51 +0100

    Work to make the subframe work with the hover UI thingy. (Not fully functional yet)

--- public/ui.js
+++ public/ui.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
 	
+	
 	$('.reaper-header form').submit(function(e) {
-		$('.reaper-dyn-page iframe').attr('src', $(this).find('.reaper-url').val());
+		$('.reaper-dyn-content').attr('src', '/' + $(this).find('.reaper-url').val());
+		
+		setTimeout(function() { 
+			subDocument = document.getElementsByClassName('reaper-dyn-content')[0].contentDocument;
+		
+			$(subDocument).ready(function() {
+			
+				console.log(subDocument);
+				
+				$(subDocument).find('*').click(function(e) {
+					$(subDocument).find('.reaper-hover-elem').removeClass('reaper-hover-elem');
+					elem = subDocument.elementFromPoint(e.clientX, e.clientY);
+					$('#overlay #viewport').css('top', $(elem).offset().top - 2).css('left', $(elem).offset().left - 2);
+					$('#overlay #viewport').css('width', $(elem).outerWidth()).css('height', $(elem).outerHeight());
+					$('#overlay').fadeIn();
+					e.preventDefault();
+					return false;
+				});
+				
+				$(subDocument).mousemove(function(e) {
+					$(subDocument).find('.reaper-hover-elem').removeClass('reaper-hover-elem');
+					elem = subDocument.elementFromPoint(e.clientX, e.clientY);
+					$(elem).addClass('reaper-hover-elem').css('outline', '1px dashed black');
+				});
+				
+				$(subDocument).find('*').mouseleave(function() {
+					$(subDocument).find('.reaper-hover-elem').removeClass('reaper-hover-elem').css('outline', '0');
+				});
+				
+			});
+		}, 500);
+		
 		return false;
 	});
 	
-	$('.wrapper').click(function(e) {
-		$('.reaper-hover-elem').removeClass('reaper-hover-elem');
-		elem = document.elementFromPoint(e.clientX, e.clientY);
-		$('#overlay #viewport').css('top', $(elem).offset().top - 2).css('left', $(elem).offset().left - 2);
-		$('#overlay #viewport').css('width', $(elem).outerWidth()).css('height', $(elem).outerHeight());
-		$('#overlay').fadeIn();
-		return false;
-	});
-	$('.wrapper').mousemove(function(e) {
-		$('.reaper-hover-elem').removeClass('reaper-hover-elem');
-		elem = document.elementFromPoint(e.clientX, e.clientY);
-		$(elem).addClass('reaper-hover-elem');
-	});
-	$('.wrapper').mouseleave(function() {
-		$('.reaper-hover-elem').removeClass('reaper-hover-elem');
-	})
 	$('#overlay').click(function() {
 		$(this).fadeOut();
 	});
